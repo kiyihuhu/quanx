637cd32b5f154a238449183493cfa08b93842a2d
kiyi <77911303+kiyihuhu@users.noreply.github.com>
2023-12-22 21:52:43 +0800

    Update kezhu.js

--- kezhu.js
+++ kezhu.js
@@ -1,8 +1,17 @@
+/****************
 [rewrite_local]
 
-^https?:\/\/imdrq\.(cn|com)\/api\/.*get_user_(subject|info|setting).* url script-response-body https://raw.githubusercontent.com/kiyihuhu/quanx/main/kezhul.js
+^https?:\/\/imdrq\.(cn|com)\/api\/.*get_user_(subject|info|setting).* url script-response-body https://raw.githubusercontent.com/kiyihuhu/quanx/main/kezhu.js
 
 
 [mitm] 
 
 hostname = .*imdrq.cn,.*imdrq.com
+/*******************************/
+var obj = JSON.parse($response.body);
+
+obj.data.vip = "1";
+
+$done({
+  body: JSON.stringify(obj),
+});
