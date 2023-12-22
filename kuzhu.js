8279c359322d1e07c6214e35628353617ae85385
kiyi <77911303+kiyihuhu@users.noreply.github.com>
2023-12-22 20:09:16 +0800

    Update kuzhu.js

--- kuzhu.js
+++ kuzhu.js
@@ -1,1 +1,19 @@
+/******************************
+[rewrite_local]
 
+https?:\/\/imdrq\.(cn|com)\/api\/.*get_user url script-response-body
+
+
+[mitm] 
+
+hostname = .*imdrq.cn,.*imdrq.com
+
+/*******************************/
+
+var obj = JSON.parse($response.body);
+
+obj.data.vip = "1";
+
+$done({
+  body: JSON.stringify(obj),
+});
